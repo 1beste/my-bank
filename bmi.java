083c9b3e3942c2961178a464a196e2d0957f7fdf
1beste <146876296+1beste@users.noreply.github.com>
2023-10-10 22:45:44 +0300

    bmi.java

---
+++ bmi.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class bmi {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Weight: ");
+        int w = input.nextInt();
+
+        System.out.print("Height: ");
+        double h = input.nextDouble();
+
+        double result = w / (h * h);
+
+        System.out.println("Result: " + result);
+
+    }
+}
